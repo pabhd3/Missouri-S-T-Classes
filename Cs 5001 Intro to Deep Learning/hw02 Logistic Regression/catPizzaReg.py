df589944064024fde43736f077b87acfd0dfd2d9
pabhd3 <pabhd3@gmail.com>
2017-10-16 21:17:53 -0500

    cs5001 hw02 Started Testing Math
    Started testing math used in the iteration loop and example loops

--- Cs 5001 Intro to Deep Learning/hw02 Logistic Regression/catPizzaReg.py
+++ Cs 5001 Intro to Deep Learning/hw02 Logistic Regression/catPizzaReg.py
@@ -1,6 +1,9 @@
 # Used to RNG
 from random import randint
 
+# Used for e^xrange
+from math import exp
+
 # Output Part 1/2
 print("CS-5001 : HW#2 : Logistic Regression.")
 print("Programmer: Peter Banko")
@@ -28,6 +31,17 @@ LLE = 0
 
 # Weights
 w = [0, 0, 0, 0, 0]
+for weight in range(0,5):
+    w[weight] = randint(-1000,1000)
+
+# Test Math
+yCap = (w[0]*1)+(w[1]*int(PEPPERONI[0]))+(w[2]*int(SAUSAGE[0]))+(w[3]*int(MUSHROOM[0]))+(w[4]*int(CHEESE[0]))
+print("\nyCap (before sigmoid) = " + str(yCap))
+yCapSigmoid = 1 / (1 + exp(-yCap))
+print("yCap (after sigmoid) = " + str(yCapSigmoid))
+ERROR = int(ACCEPT[0]) - yCapSigmoid
+print("Error = " + str(ERROR))
+
 
 # Output Part 2/2
 print("\nUsing learning rate eta = " + str(eta))
