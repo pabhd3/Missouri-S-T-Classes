9e8cd81c1774d0d19554bb019887372909bb12dd
pabhd3 <pabhd3@gmail.com>
2017-10-17 18:18:09 -0500

    cs5001 hw02 math cleanup
    Simplified and cleaned up the math used in calculations

--- Cs 5001 Intro to Deep Learning/hw02 Logistic Regression/catPizzaReg.py
+++ Cs 5001 Intro to Deep Learning/hw02 Logistic Regression/catPizzaReg.py
@@ -2,7 +2,7 @@
 from random import randint
 
 # Used for e^xrange
-from math import exp
+from math import e
 
 # Output Part 1/2
 print("CS-5001 : HW#2 : Logistic Regression.")
@@ -19,6 +19,7 @@ SAUSAGE = DATA[1::5]
 MUSHROOM = DATA[2::5]
 CHEESE = DATA[3::5]
 ACCEPT = DATA[4::5]
+X = [PEPPERONI,SAUSAGE,MUSHROOM,CHEESE]
 
 # Entries
 entries = len(ACCEPT)
@@ -39,20 +40,14 @@ print("Initial Weights = " + str(w))
 # Test Math
 for iteration in range(0, 5000):
     for entry in range(0, entries):
-        #print("\nTesting Math")
-        yCap = (w[0]*1)+(w[1]*int(PEPPERONI[entry]))+(w[2]*int(SAUSAGE[entry]))+(w[3]*int(MUSHROOM[entry]))+(w[4]*int(CHEESE[entry]))
-        #print("yCap (before sigmoid) = " + str(yCap))
-        yCapSigmoid = 1 / (1 + exp(-yCap))
-        #print("yCap (after sigmoid) = " + str(yCapSigmoid))
+        yCap = (w[0]*1)+(w[1]*int(X[0][entry]))+(w[2]*int(X[1][entry]))+(w[3]*int(X[2][entry]))+(w[4]*int(X[3][entry]))
+        yCapSigmoid = 1 / (1 + e**-yCap)
         ERROR = int(ACCEPT[entry]) - yCapSigmoid
-        #print("Error = " + str(ERROR))
-        #print("Initial Weights = " + str(w))
-        w[0] = w[0] + eta * (ERROR * yCapSigmoid)
-        w[1] = w[1] + eta * (ERROR * yCapSigmoid * int(PEPPERONI[entry]))
-        w[2] = w[2] + eta * (ERROR * yCapSigmoid * int(SAUSAGE[entry]))
-        w[3] = w[3] + eta * (ERROR * yCapSigmoid * int(MUSHROOM[entry]))
-        w[4] = w[4] + eta * (ERROR * yCapSigmoid * int(CHEESE[entry]))
-        #print("Updated Weights = " + str(w))
+        w[0] = w[0] + (eta * ERROR * yCapSigmoid * (1 - yCapSigmoid))
+        w[1] = w[1] + eta * (ERROR * yCapSigmoid * int(X[0][entry]))
+        w[2] = w[2] + eta * (ERROR * yCapSigmoid * int(X[1][entry]))
+        w[3] = w[3] + eta * (ERROR * yCapSigmoid * int(X[2][entry]))
+        w[4] = w[4] + eta * (ERROR * yCapSigmoid * int(X[3][entry]))
 
 
 # Output Part 2/2
