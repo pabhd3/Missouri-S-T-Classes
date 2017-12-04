68073a05c7a9a46cca7877b40da588a3f9e825de
pabhd3 <pabhd3@gmail.com>
2017-12-04 15:04:04 -0600

    Cs5001 Final Project: Added argparse to learn.py

---
+++ Cs 5001 Intro to Deep Learning/Final Project Q-Learning/learn.py
@@ -0,0 +1,17 @@
+# Imports needed to run
+import argparse
+
+def main():
+    # Set up Argparse
+    parser = argparse.ArgumentParser(description='Generate a Random Graph')
+    parser.add_argument('-a', metavar='Actions', type=int, default=0, 
+                        help='Flag sets number of Actions to take (Will go until Converge by default)')
+    args = parser.parse_args()
+
+    if (args.a != 0):
+        print("Actions: " + str(args.a))
+    else:
+        print("Until Converge")
+
+if __name__ == '__main__':
+    main()
