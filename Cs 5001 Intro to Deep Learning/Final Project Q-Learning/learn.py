8101ec8b3bc8eacde735c4a74df533110b1a6429
pabhd3 <pabhd3@gmail.com>
2017-12-06 15:50:47 -0600

    Cs5001 Final Project: Backtrack again

--- Cs 5001 Intro to Deep Learning/Final Project Q-Learning/learn.py
+++ Cs 5001 Intro to Deep Learning/Final Project Q-Learning/learn.py
@@ -32,7 +32,7 @@ def main():
         GRID.append([])
     for j in range(0,10):
         for k in range(0,10):
-            GRID[j].append({'info': ' ', 'up': 10, 'right': 10, 'down': 10, 'left': 10})
+            GRID[j].append({'info': ' ', 'up': 0, 'right': 0, 'down': 0, 'left': 0})
     for i in range(0,10):
         GRID[0][i]['info'] = "W"
         GRID[9][i]['info'] = "W"
@@ -70,95 +70,30 @@ def main():
 
     start_time = time.time()
     for step in range(0, args.a):
-        hund = args.a / 100
-        if(step % hund == 0):
-            print(str(step/hund) + "% Complete")
+        #hund = args.a / 100
+        #if(step % hund == 0):
+            #print(str(step/hund) + "% Complete")
         #print("Current Position: " + str(cPos))
         # Handle Tiles Falling
         tileFall = randint(0, 1)
         if(tileFall == 1):
             if(lastMove["coord"] != (0, 0)):
                 if(cPos in TILES):
-                    #print("Ouch! A Tile fell on us at ", cPos, lastMove)
-                    #print(GRID[lastMove['coord'][0]][lastMove['coord'][1]][lastMove['direction']])
+                    print("Ouch! A Tile fell on us at ", cPos, lastMove)
+                    print(GRID[lastMove['coord'][0]][lastMove['coord'][1]][lastMove['direction']])
                     GRID[lastMove['coord'][0]][lastMove['coord'][1]][lastMove['direction']] -= 10
-                    #print(GRID[lastMove['coord'][0]][lastMove['coord'][1]][lastMove['direction']])
+                    print(GRID[lastMove['coord'][0]][lastMove['coord'][1]][lastMove['direction']])
         
         # Handle Donut Falling
         if(len(activeDonuts) == 0):
             whichDonut = DONUTS[randint(0, 3)]
             activeDonuts.append(whichDonut)
             GRID[whichDonut[0]][whichDonut[1]]["info"] = "D"
-            # print("A donut fell at: " + str(whichDonut))
+            print("A donut fell at: " + str(whichDonut))
 
         # Find Best Move
-        testup = cPos[0]-1, cPos[1]
-        rewards["up"] = nextPos(GRID[testup[0]][testup[1]]['info'])
-        testright = cPos[0], cPos[1]+1
-        rewards["right"] = nextPos(GRID[testright[0]][testright[1]]['info'])
-        testdown = cPos[0]+1, cPos[1]
-        rewards["down"] = nextPos(GRID[testdown[0]][testdown[1]]['info'])
-        testleft = cPos[0], cPos[1]-1
-        rewards["left"] = nextPos(GRID[testleft[0]][testleft[1]]['info'])
-        bestReward = max(rewards, key=rewards.get)
-        GRID[cPos[0]][cPos[1]]["up"] = GRID[cPos[0]][cPos[1]]["up"] + alpha * (rewards["up"] + gamma * rewards[bestReward] - GRID[cPos[0]][cPos[1]]["up"])
-        GRID[cPos[0]][cPos[1]]["right"] = GRID[cPos[0]][cPos[1]]["right"] + alpha * (rewards["right"] + gamma * rewards[bestReward] - GRID[cPos[0]][cPos[1]]["right"])
-        GRID[cPos[0]][cPos[1]]["down"] = GRID[cPos[0]][cPos[1]]["down"] + alpha * (rewards["down"] + gamma * rewards[bestReward] - GRID[cPos[0]][cPos[1]]["down"])
-        GRID[cPos[0]][cPos[1]]["left"] = GRID[cPos[0]][cPos[1]]["left"] + alpha * (rewards["left"] + gamma * rewards[bestReward] - GRID[cPos[0]][cPos[1]]["left"])
-        rewards["up"] = GRID[cPos[0]][cPos[1]]["up"]
-        rewards["right"] = GRID[cPos[0]][cPos[1]]["right"]
-        rewards["down"] = GRID[cPos[0]][cPos[1]]["down"]
-        rewards["left"] = GRID[cPos[0]][cPos[1]]["left"]
-        bestMove = max(rewards, key=rewards.get)
-        #print(bestMove, GRID[cPos[0]][cPos[1]][bestMove])
-        #print("Movement Options: " + str(GRID[cPos[0]][cPos[1]]))
-        #print("We want to move:  " + bestReward + " with reward " + str(GRID[cPos[0]][cPos[1]][bestReward]))
 
         # Make a Move
-        stumble = randint(1,100)
-        if(stumble in range(1,82)):
-            #print("Successfully moved:  " + bestReward + " with reward " + str(GRID[cPos[0]][cPos[1]][bestReward]))
-            lastMove["coord"] = cPos
-            lastMove["direction"] = bestMove
-            print(lastMove)
-            if(bestMove == "up"):
-                cPos = (cPos[0]-1, cPos[1])
-            elif(bestMove == "right"):
-                cPos = (cPos[0], cPos[1]+1)
-            elif(bestMove == "down"):
-                cPos = (cPos[0]+1, cPos[1])
-            else:
-                cPos = (cPos[0], cPos[1]-1)
-            if(cPos in activeDonuts):
-                #print("Yay! We ate a Donut!!!")
-                activeDonuts.pop()
-                GRID[cPos[0]][cPos[1]]["info"] = " "
-        else:
-            other = True
-            while(other):
-                otherDir = MOVES[randint(0,3)]
-                if(otherDir != bestMove):
-                    stumbledTo = cPos
-                    if(otherDir == "up"):
-                        stumbledTo = (cPos[0]-1, cPos[1])
-                    elif(otherDir == "right"):
-                        stumbledTo = (cPos[0], cPos[1]+1)
-                    elif(otherDir == "down"):
-                        stumbledTo = (cPos[0]+1, cPos[1])
-                    else:
-                        stumbledTo = (cPos[0], cPos[1]-1)
-                    if(GRID[stumbledTo[0]][stumbledTo[1]]["info"] != "W"):
-                        other = False
-            #print("Stumbled:  " + otherDir + " with reward " + str(GRID[stumbledTo[0]][stumbledTo[1]][otherDir]))
-            lastMove["coord"] = cPos
-            lastMove["direction"] = otherDir
-            print(lastMove)
-            cPos = stumbledTo
-            if(cPos in activeDonuts):
-                #print("Yay! We ate a Donut!!!")
-                activeDonuts.pop()
-                GRID[cPos[0]][cPos[1]]["info"] = " "
-        #print("\n")
 
     end_time = time.time()
     elapsed_time = end_time - start_time
